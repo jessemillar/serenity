42213fdc76a1edf8d3693b8bf348f7a8e890957d
Jesse Millar <hellojessemillar@gmail.com>
2018-01-12 16:57:58 -0600

    Support for more data

--- helpers/sqlite.go
+++ helpers/sqlite.go
@@ -7,16 +7,16 @@ import (
 )
 
 type Book struct {
-	Title string `json:"title"`
-	//Subtitle string
-	Author string `json:"author"`
-	//Synopsis string
-	LCC *string `json:"lcc,omitempty"`
-	//Publisher string
-	//ISBN int
-	//Genre string
-	//PageCount int
-	//PublishYear int
+	Title       string  `json:"title"`
+	Subtitle    *string `json:"subtitle,omitempty"`
+	Author      string  `json:"author"`
+	Genre       string  `json:"genre"`
+	Synopsis    *string `json:"synopsis,omitempty"`
+	LCC         *string `json:"lcc,omitempty"`
+	ISBN        int     `json:"isbn"`
+	Publisher   *string `json:"publisher,omitempty"`
+	PublishYear int     `json:"publishYear"`
+	PageCount   *int    `json:"pageCount,omitempty"`
 }
 
 func InitDB(filepath string) *sql.DB {
@@ -32,7 +32,7 @@ func InitDB(filepath string) *sql.DB {
 
 func ReadItem(db *sql.DB) []Book {
 	sql_readall := `
-	SELECT ZTITLE, ZDISPLAYNAME, ZLCC FROM ZBOOK
+	SELECT ZTITLE, ZSUBTITLE, ZDISPLAYNAME, ZGENRE, ZSYNOPSIS, ZLCC, ZISBN, ZPUBLISHER, ZPUBLISHYEAR, ZPAGECOUNT FROM ZBOOK
 	INNER JOIN ZAUTHOR ON ZBOOK.ZAUTHORINFO=ZAUTHOR.Z_PK;
 `
 
@@ -45,7 +45,7 @@ func ReadItem(db *sql.DB) []Book {
 	var allBooks []Book
 	for rows.Next() {
 		book := Book{}
-		err2 := rows.Scan(&book.Title, &book.Author, &book.LCC)
+		err2 := rows.Scan(&book.Title, &book.Subtitle, &book.Author, &book.Genre, &book.Synopsis, &book.LCC, &book.ISBN, &book.Publisher, &book.PublishYear, &book.PageCount)
 		if err2 != nil {
 			panic(err2)
 		}
